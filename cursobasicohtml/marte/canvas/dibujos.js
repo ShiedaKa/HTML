d038be109a07649d11ed5f82404220059a1892e9
shiedaka <rousefloremiranda@gmail.com>
2022-04-08 14:30:02 -0500

    correpciones

--- cursobasicohtml/marte/canvas/dibujos.js
+++ cursobasicohtml/marte/canvas/dibujos.js
@@ -2,15 +2,22 @@ var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
 var lineas = 30;
 var l = 0;
-var yi, xf;
+var yi, xf, xi, yf;
 
 while (l < lineas) 
 {
-  yi=10 * l;
-  xf=10(l + 1);
-  dibujarLinea("#AAF", 0, yi, xf, 300);
+   yi= 10 * l;
+   xf= 10 * (l + 1);
+  dibujarLinea("#e11a29", 0, yi, xf, 300);
   console.log("linea" +l);
   l = l +1;
+  
+  yf= 10 * l;
+  xi= 10 * (l + 1);
+  dibujarLinea("#6fc6d7", xi, 0, 300, yf);
+  console.log("linea" +l);
+  l = l +1;
+  
 }
 
 dibujarLinea("#AAF", 1,1,1,299);
@@ -18,12 +25,12 @@ dibujarLinea("#AFA", 1,299,299,299);
 dibujarLinea("#AFA", 299,299,299,1);
 dibujarLinea("#AFA", 299,1,1,1);
 
-function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal);
+function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
 {
     lienzo.beginPath();
     lienzo.strokeStyle = color;
     lienzo.moveTo(xinicial, yinicial);
     lienzo.lineTo(xfinal, yfinal);
     lienzo.stroke();
-    lienzo.colsePath();
+    lienzo.closePath();
 }
