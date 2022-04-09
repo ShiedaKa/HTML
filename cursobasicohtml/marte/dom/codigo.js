6c2561df387e395118619695f7f75e5a3fe280f2
shiedaka <rousefloremiranda@gmail.com>
2022-04-08 20:12:46 -0500

    cambios

---
+++ cursobasicohtml/marte/dom/codigo.js
@@ -0,0 +1,9 @@
+alert("Si funciona!!");
+var ruta = window.location; //var ruta = la ruta de los archivos en HD
+console.log(ruta);
+document.write("Estas en: " +ruta);
+
+//calcula peso
+
+
+//fin calcula peso
